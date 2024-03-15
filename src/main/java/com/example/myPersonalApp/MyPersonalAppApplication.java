7ddf9889b82eb870e54ac88452150a572db2c618
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-15 21:46:55 +0100

    created project

---
+++ src/main/java/com/example/myPersonalApp/MyPersonalAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myPersonalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myPersonalApp/MyPersonalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myPersonalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
