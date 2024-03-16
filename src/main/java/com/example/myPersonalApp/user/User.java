86df365e9174b41f19e173fa4d823574af1d1628
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-16 21:40:26 +0100

    user entity don

--- src/main/java/com/example/myPersonalApp/user/User.java
+++ src/main/java/com/example/myPersonalApp/user/User.java
@@ -1,4 +1,26 @@
 package com.example.myPersonalApp.user;
 
+import com.example.myPersonalApp.enums.Role;
+import com.example.myPersonalApp.talk.Talk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private Role role;
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Talk> talks;
 }
