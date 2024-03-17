3692b86eecda99d417e2e354f71580e4e1c1a24a
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-17 21:28:40 +0100

    immagini entity done

--- src/main/java/com/example/myPersonalApp/immagini/Immagini.java
+++ src/main/java/com/example/myPersonalApp/immagini/Immagini.java
@@ -1,4 +1,19 @@
 package com.example.myPersonalApp.immagini;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.scheduling.config.Task;
+
+@Entity
+@Table(name="immagini")
+@Data
 public class Immagini {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    private String link;
+    @ManyToOne
+    @JsonIgnore
+    private Task task;
 }
