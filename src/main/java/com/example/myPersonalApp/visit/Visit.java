a8a8e550b5c45e522943be264a16b618b0a57b82
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-17 21:29:37 +0100

    created visit entity

--- src/main/java/com/example/myPersonalApp/visit/Visit.java
+++ src/main/java/com/example/myPersonalApp/visit/Visit.java
@@ -1,4 +1,16 @@
 package com.example.myPersonalApp.visit;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
 public class Visit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate data;
 }
