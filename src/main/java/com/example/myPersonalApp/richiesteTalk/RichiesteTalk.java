b0927ebe93dfe932fc9d8f4615236797bcd50686
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-17 21:33:13 +0100

    created richieste talk

--- src/main/java/com/example/myPersonalApp/richiesteTalk/RichiesteTalk.java
+++ src/main/java/com/example/myPersonalApp/richiesteTalk/RichiesteTalk.java
@@ -1,4 +1,20 @@
 package com.example.myPersonalApp.richiesteTalk;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="richiesteTalk")
+@Data
 public class RichiesteTalk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String linkProfilo;
+    private String professione;
+    private int eta;
+    private String testo;
 }
