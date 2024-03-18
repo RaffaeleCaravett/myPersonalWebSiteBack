e69fa4d3bbf3ded2bc59045eb86aaa74bfd59f4f
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-18 21:12:54 +0100

    added stuff in auth controller and service

--- src/main/java/com/example/myPersonalApp/auth/AuthController.java
+++ src/main/java/com/example/myPersonalApp/auth/AuthController.java
@@ -1,9 +1,60 @@
 package com.example.myPersonalApp.auth;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.myPersonalApp.user.User;
+import com.example.myPersonalApp.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private UserService utenteService;
+
+
+
+    @PostMapping("/login")
+    public UserLoginSuccessDTO login(@RequestBody UserLoginDTO body) throws Exception {
+
+        return new UserLoginSuccessDTO(authService.authenticateUser(body));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public User saveUser(@RequestBody @Validated UserRegistrationDTO body, BindingResult validation){
+        if(validation.hasErrors()){
+            throw new BadRequestException(validation.getAllErrors());
+        } else {
+            try {
+                return authService.registerUser(body);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @GetMapping("/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    public User verifyToken(@PathVariable String token){
+        return jwtTools.verifyToken(token);
+    }
+    @GetMapping("/refreshToken/{refreshToken}")
+    @ResponseStatus(HttpStatus.OK)
+    public Token verifyRefreshToken(@PathVariable String refreshToken){
+        return jwtTools.verifyRefreshToken(refreshToken);
+    }
+
+
+
 }
--- src/main/java/com/example/myPersonalApp/auth/AuthService.java
+++ src/main/java/com/example/myPersonalApp/auth/AuthService.java
@@ -1,7 +1,60 @@
 package com.example.myPersonalApp.auth;
 
+import com.example.myPersonalApp.user.User;
+import com.example.myPersonalApp.user.UserRepository;
+import com.example.myPersonalApp.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class AuthService {
+    @Autowired
+    private UserService usersService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+
+    public Token authenticateUser(UserLoginDTO body) throws Exception {
+        // 1. Verifichiamo che l'email dell'utente sia nel db
+        User user = usersService.findByEmail(body.email());
+        // 2. In caso affermativo, verifichiamo se la password corrisponde a quella trovata nel db
+        if(bcrypt.matches(body.password(), user.getPassword()))  {
+            // 3. Se le credenziali sono OK --> Genero un JWT e lo restituisco
+            return jwtTools.createToken(user);
+        } else {
+            // 4. Se le credenziali NON sono OK --> 401
+            throw new UnauthorizedException("Credenziali non valide!");
+        }
+    }
+
+
+    public User registerUser(UserRegistrationDTO body) throws IOException {
+
+        // verifico se l'email è già utilizzata
+        userRepository.findByEmail(body.email()).ifPresent( user -> {
+            throw new BadRequestException("L'email " + user.getEmail() + " è già utilizzata!");
+        });
+        User newUser = new User();
+        newUser.setPassword(bcrypt.encode(body.password()));
+        newUser.setEmail(body.email());
+        newUser.setName(body.nome());
+        newUser.setSurname(body.cognome());
+        newUser.setRole(Role.ADMIN);
+        userRepository.save(newUser);
+
+        return newUser;
+    }
+
+
+
 }
