3c7617525a2d4404d88d12e558259af50fbe31ad
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-19 21:36:09 +0100

    added preAuthorization to controllers 'user' and 'richiesteTalk'

--- src/main/java/com/example/myPersonalApp/richiesteTalk/RichiesteTalkController.java
+++ src/main/java/com/example/myPersonalApp/richiesteTalk/RichiesteTalkController.java
@@ -5,6 +5,7 @@ import com.example.myPersonalApp.exceptions.BadRequestException;
 import com.example.myPersonalApp.payloads.entities.RichiesteTalkDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class RichiesteTalkController {
 
 
 @PostMapping("")
+    @PreAuthorize("hasAuthority('Admin')")
     public RichiesteTalk save (@RequestBody @Validated RichiesteTalkDTO richiesteTalkDTO, BindingResult validation){
     if(validation.hasErrors()){
         throw new BadRequestException(validation.getAllErrors());
@@ -24,18 +26,21 @@ public class RichiesteTalkController {
     return richiesteTalkService.save(richiesteTalkDTO);
 }
 @GetMapping("")
+    @PreAuthorize("hasAuthority('Admin')")
     public Page<RichiesteTalk> getAllPaginated(@RequestParam(defaultValue = "1") int page,@RequestParam(defaultValue = "10") int size,
                                                @RequestParam(defaultValue = "id") String orderBy){
     return richiesteTalkService.getAllPaginated(page,size,orderBy);
 }
 
 @GetMapping("/categoria")
+    @PreAuthorize("hasAuthority('Admin')")
     public Page<RichiesteTalk> getAllPaginatedByCategory(@RequestParam(defaultValue = "1") int page,@RequestParam(defaultValue = "10") int size,
                                                          @RequestParam(defaultValue = "id") String orderBy,@RequestParam(defaultValue = "Fitness") String categoria){
     return richiesteTalkService.getAllPaginatedByCategory(page,size,orderBy, Categoria.valueOf(categoria));
 }
 
 @GetMapping("/leggi/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
     public RichiesteTalk leggi(@PathVariable long id){
     return richiesteTalkService.leggi(id);
 }
--- src/main/java/com/example/myPersonalApp/user/UserController.java
+++ src/main/java/com/example/myPersonalApp/user/UserController.java
@@ -2,6 +2,7 @@ package com.example.myPersonalApp.user;
 
 import com.example.myPersonalApp.payloads.entities.UserRegistrationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,15 +14,18 @@ public class UserController {
 
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
     public User getById(@PathVariable long id){
         return userService.findById(id);
     }
     @PutMapping("/me")
+    @PreAuthorize("hasAuthority('Admin')")
     public User putById(@AuthenticationPrincipal User currentUser, @RequestBody UserRegistrationDTO userRegistrationDTO){
         return userService.putById(currentUser.getId(),userRegistrationDTO);
     }
 
     @DeleteMapping("/me")
+    @PreAuthorize("hasAuthority('Admin')")
     public boolean deleteById(@AuthenticationPrincipal User currentUser){
         return userService.deleteById(currentUser.getId());
     }
