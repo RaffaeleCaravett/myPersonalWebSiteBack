964ee05351b9e550d02b3a8ba216e2af16240813
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-20 21:51:23 +0100

    created talk service and talk controller

---
+++ src/main/java/com/example/myPersonalApp/talk/TalkController.java
@@ -0,0 +1,9 @@
+package com.example.myPersonalApp.talk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/talk")
+public class TalkController {
+}
---
+++ src/main/java/com/example/myPersonalApp/talk/TalkService.java
@@ -0,0 +1,7 @@
+package com.example.myPersonalApp.talk;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TalkService {
+}
