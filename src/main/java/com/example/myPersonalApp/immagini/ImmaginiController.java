9db20cdd05d917afd29ea4fe9a9f449eb251b129
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-21 21:43:12 +0100

    added preAuthorize to immagini controller

--- src/main/java/com/example/myPersonalApp/immagini/ImmaginiController.java
+++ src/main/java/com/example/myPersonalApp/immagini/ImmaginiController.java
@@ -3,6 +3,7 @@ package com.example.myPersonalApp.immagini;
 import com.example.myPersonalApp.exceptions.BadRequestException;
 import com.example.myPersonalApp.payloads.entities.ImmaginiDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class ImmaginiController {
 
 
     @PostMapping("")
+        @PreAuthorize("hasAuthority('Admin')")
     public Immagini save(@RequestBody @Validated ImmaginiDTO immaginiDTO, BindingResult validation, @RequestParam("immagine") MultipartFile body) throws IOException {
        if(validation.hasErrors()){
            throw new BadRequestException(validation.getAllErrors());
@@ -25,6 +27,7 @@ public class ImmaginiController {
         return immaginiService.save(immaginiDTO,body);
     }
     @PutMapping("/{id}/{talk_id}")
+        @PreAuthorize("hasAuthority('Admin')")
     public Immagini putById(@RequestBody ImmaginiDTO immaginiDTO,BindingResult validation,@PathVariable long id,@PathVariable long talk_id,@RequestParam("immagine") MultipartFile file){
      if(validation.hasErrors()){
          throw new BadRequestException(validation.getAllErrors());
@@ -32,6 +35,7 @@ public class ImmaginiController {
      return immaginiService.putById(id,talk_id,immaginiDTO,file);
     }
 @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
     public boolean deleteById(@PathVariable long id){
         return immaginiService.deleteById(id);
 }
