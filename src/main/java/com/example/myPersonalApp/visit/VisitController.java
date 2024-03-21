171eda5494569b8af7fe8e677338b5e39b4c35cc
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-21 21:51:08 +0100

    visit controller done

--- src/main/java/com/example/myPersonalApp/visit/VisitController.java
+++ src/main/java/com/example/myPersonalApp/visit/VisitController.java
@@ -1,9 +1,29 @@
 package com.example.myPersonalApp.visit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("")
+@RequestMapping("/visit")
 public class VisitController {
+
+
+    @Autowired
+    VisitService visitService;
+
+
+    @PostMapping("")
+    public Visit save(){
+        return visitService.save();
+    }
+
+    @GetMapping("")
+    public List<Visit> getAll(){
+        return visitService.getAll();
+    }
 }
